e8f4a3eafc28d03710b0f3ddd40da524e8d28202
coneseo <woon4754@gmail.com>
2019-02-14 02:18:43 +0900

    add config package and login.html

---
+++ src/main/java/com/example/shoppingmall/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.shoppingmall.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .requestMatchers(new AntPathRequestMatcher("/**.html"))
+                .requestMatchers(new AntPathRequestMatcher("/static/**"));
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .logout() // logout설정
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/main")
+                .permitAll().and()
+                .authorizeRequests() // 인가에 대한 설정
+                .antMatchers("/users/delete").permitAll()
+                .antMatchers("/users/join").permitAll()
+                .antMatchers("/users/welcome").permitAll()
+                .antMatchers("/users/login").permitAll()
+                .antMatchers("/users/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/posts/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/main").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin() // 사용자가 정의하는 로그인 화면을 만들겠다.
+                .loginProcessingUrl("/users/login") // 로그인 화면
+                .loginPage("/users/login") // 사용자가 입력한 id, password가 전달되는 url경로(필터가처리)
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .failureUrl("/users/login?fail=true");
+    }
+}
